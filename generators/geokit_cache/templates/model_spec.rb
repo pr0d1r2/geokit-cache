34c03047964244015280b2634d7d802b911076dd
Marcin Nowicki <pr0d1r2@ragnarson.com>
2009-09-24 12:47:12 +0200

    Replace generator for testing with sufficent one.

--- generators/geokit_cache/templates/model_spec.rb
+++ generators/geokit_cache/templates/model_spec.rb
@@ -2,16 +2,8 @@ require File.expand_path(File.dirname(__FILE__) + '<%= '/..' * class_nesting_dep
 
 describe <%= class_name %> do
 
-  before(:each) do
-    @valid_attributes = {
-<% attributes.each_with_index do |attribute, attribute_index| -%>
-      :<%= attribute.name %> => <%= attribute.default_value %><%= attribute_index == attributes.length - 1 ? '' : ','%>
-<% end -%>
-    }
-  end
-
-  it "should create a new instance given valid attributes" do
-    <%= class_name %>.create!(@valid_attributes)
+  it "should provide geocode command" do
+    <%= class_name %>.should respond_to(:geocode)
   end
 
 end
